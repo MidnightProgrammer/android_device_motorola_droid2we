3932dde0c742401f2c2f46c88d78d2000b720317
Midnight Programmer <midnightprogrammer@gmail.com>
2012-01-10 16:23:16 -0500

    android device motorola droid2we

---
+++ BoardConfig.mk
@@ -0,0 +1,145 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := false
+BOARD_USE_LEGACY_TOUCHSCREEN := true
+
+# inherit from the proprietary version
+-include vendor/motorola/droid2we/BoardConfigVendor.mk
+
+# use pre-kernel.35 vold usb mounting
+#BOARD_USE_USB_MASS_STORAGE_SWITCH := true
+
+# Processor
+TARGET_NO_BOOTLOADER := false
+TARGET_BOARD_PLATFORM := omap3
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_BOOTLOADER_BOARD_NAME := droid2we
+TARGET_CPU_SMP := false
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a8
+TARGET_ARCH_VARIANT_FPU := neon
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8
+
+
+# Kernel
+TARGET_PREBUILT_KERNEL := device/moto/droid2we/kernel
+BOARD_KERNEL_CMDLINE := console=ttyS2,115200n8 rw mem=498M@0x80C00000 init=/init ip=off brdrev=P3A androidboot.bootloader=0x0000 mmcparts=mmcblk1:p7(pds),p15(boot),p16(recovery),p17(cdrom),p18(misc),p19(cid),p20(kpanic),p21(system),p22(cache),p23(preinstall),p24(userdata) androidboot.mode=reboot androidboot.bootloader=D011 androidboot.serialno=0A3A94CF0602D02
+BOARD_KERNEL_BASE := 0x10000000
+
+# Sensors
+ENABLE_SENSORS_COMPAT := true
+
+# Connectivity - Wi-Fi
+BOARD_WPA_SUPPLICANT_DRIVER := CUSTOM
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := libCustomWifi
+WPA_SUPPLICANT_VERSION      := VER_0_6_X
+BOARD_WLAN_DEVICE           := wl1271
+BOARD_WLAN_TI_STA_DK_ROOT   := hardware/ti/wlan/wl1271
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/tiwlan_drv.ko"
+WIFI_DRIVER_MODULE_NAME     := "tiwlan_drv"
+WIFI_DRIVER_MODULE_ARG      := ""
+WIFI_FIRMWARE_LOADER        := "wlan_loader"
+WIFI_DRIVER_FW_STA_PATH     := "/system/etc/wifi/fw_wlan1271.bin"
+WIFI_DRIVER_FW_AP_PATH      := "/system/etc/wifi/fw_tiwlan_ap.bin"
+
+# Audio
+BOARD_USES_GENERIC_AUDIO := false
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# Recovery
+BOARD_HAS_LOCKED_BOOTLOADER := true
+TARGET_PREBUILT_RECOVERY_KERNEL := device/moto/droid2we/kernel
+BOARD_ALWAYS_INSECURE := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_MKE2FS := device/moto/droid2we/releaseutils/mke2fs
+TARGET_RECOVERY_PRE_COMMAND := "echo 1 > /data/.recovery_mode; sync;"
+TARGET_RECOVERY_PRE_COMMAND_CLEAR_REASON := true
+
+#BOARD_BOOTIMAGE_PARTITION_SIZE := 0x003fffff
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x004fffff
+#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0bdfffff   # limited so we enforce room to grow
+#BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+
+# Graphics
+BOARD_EGL_CFG := device/moto/droid2we/prebuilt/etc/egl.cfg
+COMMON_GLOBAL_CFLAGS += -DMISSING_EGL_EXTERNAL_IMAGE -DMISSING_EGL_PIXEL_FORMAT_YV12 -DMISSING_GRALLOC_BUFFERS
+BOARD_NO_RGBX_8888 := true
+DEFAULT_FB_NUM := 0
+BOARD_USE_YUV422I_DEFAULT_COLORFORMAT := true
+
+# OMX
+HARDWARE_OMX := true
+ifdef HARDWARE_OMX
+OMX_VENDOR := ti
+OMX_VENDOR_WRAPPER := TI_OMX_Wrapper
+BOARD_OPENCORE_LIBRARIES := libOMX_Core
+BOARD_OPENCORE_FLAGS := -DHARDWARE_OMX=1
+endif
+OMX_TI_OMAP_TIER_LEVEL := 10
+
+# OMAP
+OMAP_ENHANCEMENT := true
+ifdef OMAP_ENHANCEMENT
+COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT -DTARGET_OMAP3
+endif
+
+
+# MOTOROLA
+USE_MOTOROLA_CODE := true
+ifdef USE_MOTOROLA_CODE
+COMMON_GLOBAL_CFLAGS += -DUSE_MOTOROLA_CODE
+endif
+USE_MOTOROLA_USERS := true
+ifdef USE_MOTOROLA_USERS
+COMMON_GLOBAL_CFLAGS += -DUSE_MOTOROLA_USERS
+endif
+
+# OTA Packaging
+TARGET_PROVIDES_RELEASETOOLS := true
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/moto/droid2we/releasetools/droid2we_ota_from_target_files
+TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := device/moto/droid2we/releasetools/droid2we_img_from_target_files
+
+# UMS
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/usb_mass_storage/lun%d/file"
+
+# Hijack
+#TARGET_NEEDS_MOTOROLA_HIJACK := true
+#BOARD_HIJACK_LOG_ENABLE := true
+
+# Misc.
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_NEEDS_CUTILS_LOG := true
+BOARD_USES_SECURE_SERVICES := true
+BOARD_USE_BATTERY_CHARGE_COUNTER := true
+BOARD_ALWAYS_INSECURE := true
+
+DROID2WE_HARDWARE := true
+BOARD_GLOBAL_CFLAGS += -DDROID2WE_HARDWARE
